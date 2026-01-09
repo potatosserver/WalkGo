88e42739941caee634e826698ee1679ec5349808
potatosserver <potatosserver@gmail.com>
2026-01-09 05:53:58 +0000

    更新

--- android/app/src/main/kotlin/com/potatsserver/walkgo/MainActivity.kt
+++ android/app/src/main/kotlin/com/potatsserver/walkgo/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.potatsserver.walkgo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "foreground_service",
+                "Foreground Service",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "This channel is used for important notifications."
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
