349dda6eb86185b84c9c8393a3a395a2de61541e
andrewcho365 <andrewcho365@gmail.com>
2026-01-09 04:39:48 +0000

    update

--- lib/permission_service.dart
+++ lib/permission_service.dart
@@ -5,6 +5,13 @@ class PermissionService {
   Future<bool> hasAllPermissions() async {
     final activityStatus = await Permission.activityRecognition.status;
     final notificationStatus = await Permission.notification.status;
-    return activityStatus.isGranted && notificationStatus.isGranted;
+    final batteryStatus = await Permission.ignoreBatteryOptimizations.status;
+    return activityStatus.isGranted && notificationStatus.isGranted && batteryStatus.isGranted;
+  }
+
+  Future<void> requestPermissions() async {
+    await Permission.activityRecognition.request();
+    await Permission.notification.request();
+    await Permission.ignoreBatteryOptimizations.request();
   }
 }
