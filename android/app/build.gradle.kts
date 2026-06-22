9387a79810896b90bcf743220cc75f5230e8ed97
AndrewCho <andrewcho365@gmail.com>
2026-06-22 08:57:04 +0000

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 import java.util.Properties
 import java.io.FileInputStream
 import java.io.File
@@ -53,6 +54,9 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
+            ndk {
+                debugSymbolLevel = "NONE"
+            }
         }
     }
 }
