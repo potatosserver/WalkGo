5137df63e724ed85eb044bc640fcfa54fb0e5a7b
AndrewCho <andrewcho365@gmail.com>
2026-06-23 14:18:54 +0000

    icon update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,13 +2,14 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "nixos-26.05"; # or "stable-24.05"
+  channel = "stable-25.05";
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.jdk21
     pkgs.unzip
     pkgs.gh
     pkgs.flutter
+    pkgs.tree
   ];
   # Sets environment variables in the workspace
   env = {};
